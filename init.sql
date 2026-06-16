c31e00505d3976e079017bbdcd5ea836e9eb42de
FABIO SEBASTIAN UPIACHIHUA LOZANO <fabio.upiachihua@unas.edu.pe>
2026-06-15 20:17:08 -0500

    Creación de tablas de usuarios y auditoría

---
+++ init.sql
@@ -0,0 +1,15 @@
+\i Database/modules/roles.sql
+\i Database/modules/usuarios.sql
+\i Database/modules/usuarios_roles.sql
+\i Database/modules/auditoria.sql
+--\i modules/catalogos.sql
+--\i modules/sistemas.sql
+--\i modules/integraciones.sql
+
+--\i modules/arquitectura.sql
+--\i modules/infraestructura.sql
+--\i modules/seguridad.sql
+
+--\i modules/evidencias.sql
+--\i modules/validaciones.sql
+--\i modules/observaciones.sql
